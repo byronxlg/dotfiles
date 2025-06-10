f9a424b586db730d32bd66fc9b942c9c3f196467
byronxlg <byron.lg.smith@gmail.com>
2025-06-11 09:11:57 +1200

    feat: run MacOS settings from script

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # Get Started
 
-## Install Brew Libraries
+## MacOS Setup
+
+-   Install Brew Libraries
+-   MacOS Settings
+
 ```bash
-chmod +x ./.config/zsh/setup_macos.sh && ./.config/zsh/setup_macos.sh
+chmod +x ./setup_macos.sh && ./setup_macos.sh
 ```
 
 ## Terminal Setup (MacOS)
 
-- [iTerm2](https://iterm2.com/)
-- [Nerd Font](https://www.nerdfonts.com/font-downloads)
-- [Color Scheme](https://github.com/catppuccin/iterm)
+-   [iTerm2](https://iterm2.com/)
+-   [Nerd Font](https://www.nerdfonts.com/font-downloads)
+-   [Color Scheme](https://github.com/catppuccin/iterm)
 
 ## Neovim
 
-- [Neovim Build with CMAKE](https://github.com/neovim/neovim/wiki/Building-Neovim/f0a248bbbf7c387e173f3456e624b53e9f7d6f1d#build-prerequisites)
+-   [Neovim Build with CMAKE](https://github.com/neovim/neovim/wiki/Building-Neovim/f0a248bbbf7c387e173f3456e624b53e9f7d6f1d#build-prerequisites)
 
 ## Lazy.Nvim
 
-- [lazy.nvim](https://lazy.folke.io/)
+-   [lazy.nvim](https://lazy.folke.io/)
---
+++ setup_macos.sh
@@ -0,0 +1,65 @@
+packages=(
+    # Zsh
+	'zsh-autosuggestions'
+	'zsh-syntax-highlighting'
+	'starship'
+	'bat'
+	'stow'
+	'ruff'
+	'tree'
+	'fzf'
+	'fd'
+	'tmux'
+	'watch'
+	'lsd'
+	'vivid'
+	'zoxide'
+
+	# MacOS
+	iterm2
+	koekeishiya/formulae/yabai # yabai --start-service / yabai --restart-service
+	koekeishiya/formulae/skhd  # skhd --start-service / skhd --restart-service
+
+
+    # Neovim and dependencies
+	'lua'
+	'luarocks'
+
+    # Other
+	'wget'
+	'lazygit'
+	'jq'
+	'grep'
+)
+
+# Install with Homebrew
+for package in "${packages[@]}"; do
+    brew install $package
+done
+
+# Instantly hide/ reveal the dock
+defaults write com.apple.Dock autohide-delay -float 0 && killall Dock
+defaults write com.apple.dock autohide-time-modifier -float 0 && killall Dock
+
+# Untested
+# defaults write -g NSScrollViewRubberbanding -int 0
+# defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+# defaults write -g NSScrollAnimationEnabled -bool false
+# defaults write -g NSWindowResizeTime -float 0.001
+# defaults write -g QLPanelAnimationDuration -float 0
+# defaults write -g NSScrollViewRubberbanding -bool false
+# defaults write -g NSDocumentRevisionsWindowTransformAnimation -bool false
+# defaults write -g NSToolbarFullScreenAnimationDuration -float 0
+# defaults write -g NSBrowserColumnAnimationSpeedMultiplier -float 0
+# defaults write com.apple.dock autohide-time-modifier -float 0
+# defaults write com.apple.dock autohide-delay -float 0
+# defaults write com.apple.dock expose-animation-duration -float 0
+# defaults write com.apple.dock springboard-show-duration -float 0
+# defaults write com.apple.dock springboard-hide-duration -float 0
+# defaults write com.apple.dock springboard-page-duration -float 0
+# defaults write com.apple.finder DisableAllAnimations -bool true
+# defaults write com.apple.Mail DisableSendAnimations -bool true
+# defaults write com.apple.Mail DisableReplyAnimations -bool true
+# defaults write NSGlobalDomain NSWindowResizeTime .001
+# defaults write com.apple.dock expose-animation-duration -int 0; killall Dock
+# defaults write com.apple.dock expose-animation-duration -float 0.1; killall Dock
