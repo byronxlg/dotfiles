5c1d182b2f52365b1bcf593a1d5fac443a57f4b3
Byron Smith <byron.smith@slalom.com>
2026-01-15 10:58:57 +1300

    claude uninstall

--- .claude/uninstall_claude_native.sh
+++ .claude/uninstall_claude_native.sh
@@ -1,3 +1,14 @@
-rm -rf ~/.claude
+# Uninstall
 rm ~/.local/bin/claude
 rm -rf ~/.local/share/claude
+
+# User-level Claude Code config:
+rm -rf ~/.claude
+rm -f ~/.claude.json ~/.claude.json.backup
+
+# Claude Desktop preferences:
+rm -f ~/Library/Preferences/com.anthropic.claudefordesktop.plist
+
+
+# Install
+# curl -fsSL https://claude.ai/install.sh | bash
